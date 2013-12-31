4ad195f19c9fbc91e42a11872e3658677c5c00a3
Travis Fields <tfields@commercehub.com>
2013-12-31 14:59:34 -0500

    added more documentation

--- Modulefile
+++ Modulefile
@@ -5,7 +5,7 @@ author 'Travis F'
 license 'Apache License, Version 2.0'
 summary 'Powershell Alternative to wget or curl'
 description 'Tired of Unix dependencies in your Windows Environment to simply download web artifacts on your servers, use PGet it utilizes Powershell as and removes other dependencies'
-project_page 'UNKNOWN'
+project_page 'http://github.com/cyberious/puppet-pget'
 
 ## Add dependencies, if any:
 dependency 'joshcooper/powershell', '>= 0.6.0'
--- manifests/init.pp
+++ manifests/init.pp
@@ -6,34 +6,33 @@
 #
 # Document parameters here.
 #
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
+# [*source*]
+#   URL which you want to download from
+# [*target*]
+#   The stage directory where the file is to be downloaded
+# [*username*]
+#   If authentications is required provide both username and password
+# [*password*]
+#   Optional only if username is not supplied
 #
 # === Examples
 #
-#  class { pget:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
+#  pget{'Download puppet':
+#source  => "http://downloads.puppetlabs.com/windows/puppet-3.4.1.msi",
+#target  => "C:/software",
+#}
+
+# pget{'Download puppet':
+#   source  => "http://downloads.puppetlabs.com/windows/puppet-3.4.1.msi",
+#   target  => "C:/software",
+#   username=> "myuser",
+#   password=> "password1'
+# }
 #
-# === Authors
-#
-# Author Name <author@domain.com>
 #
 # === Copyright
 #
-# Copyright 2013 Your name here, unless otherwise noted.
+# Copyright 2013 Travis F, unless otherwise noted.
 #
 define pget (
   $source,              #: the source file location, supports local files, puppet://, http://, https://, ftp://
