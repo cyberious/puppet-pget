ed3b9407d64e6b5749c6e8dc503b71f8dc637b1c
Andrey Panteleyev <insonifi@gmail.com>
2014-02-22 15:41:06 +0400

    replace URI Class with plain Regexp.

--- lib/puppet/parser/functions/pget_filename.rb
+++ lib/puppet/parser/functions/pget_filename.rb
@@ -1,5 +1,3 @@
-require 'uri'
-
 module Puppet::Parser::Functions
   newfunction(:pget_filename, :type  => :rvalue, :doc => <<-EOS
 Returns the filename of a path
@@ -8,7 +6,7 @@ Returns the filename of a path
     raise(Puppet::ParseError, "pget_filename(): Wrong number of arguments " +
         "given (#{args.size} for 1") if args.size != 1
     path = args[0]
-    return File.basename(URI(path))
+    return path[/.*\/(?<filename>[^\/#?]+)/, -1]
   end
 end
 
