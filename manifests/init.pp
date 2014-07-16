6ed5fc478b1f4cadce960388195bb94c3cae0d7c
Travis Fields <travis@puppetlabs.com>
2014-07-15 22:12:18 -0700

    Add md5 checks

--- manifests/init.pp
+++ manifests/init.pp
@@ -41,26 +41,28 @@ define pget (
   $target      = undef, #: the target stage directory
   $username    = undef, #: Username to be passed
   $password    = undef, #: password needed,
-  $timeout     = 300,   #: timeout
   $headerHash  = undef, #: additional has parameters for the download of the file, i.e. user-agent, Cookie
-  ){
+  $md5Hash     = undef, #: MD5 checksum of the file
+  $timeout     = 300,   #: timeout
+){
 
   validate_string($source)
-  validate_re($source,['^s?ftp:','^https?:','^ftps?:','^puppet:'])
+  validate_re($source, ['^s?ftp:','^https?:','^ftps?:','^puppet:'])
 
   if $::operatingsystem != 'windows'{
     fail("Unsupported OS ${::operatingsystem}")
   }
+
   $filename = pget_filename($source)
   $target_file = "${target}/${filename}"
 
   if $source =~ /^puppet/ {
-    file{"Download-${filename}":
+    file{ "Download-${filename}":
       ensure  => file,
       path    => $target_file,
       source  => $source,
     }
-  }else{
+  } else{
     validate_re($source,['^s?ftp:','^https?:','^ftps?:'])
     $base_cmd = '$wc = New-Object System.Net.WebClient;'
     if $username or $password {
@@ -77,10 +79,10 @@ define pget (
     }
     $cmd = "${base_cmd}${header_cmd}${pass_cmd}\$wc.DownloadFile('${source}','${target_file}')"
     debug("About to execute command ${cmd}")
-    exec{"Download-${filename}":
+    exec{ "Download-${filename}":
       provider  => powershell,
       command   => $cmd,
-      unless    => "if(Test-Path -Path \"${target_file}\" ){ exit 0 }else{exit 1}",
+      unless    => template("pget/filecheck.ps1.erb"),
       timeout   => $timeout,
     }
   }
